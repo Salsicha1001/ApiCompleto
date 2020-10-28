386f5b47725bed0960de8f7d6bb0eaf06c745914
Felipe Borges <felumais14@gmail.com>
2020-10-28 14:56:21 -0300

    first commit

---
+++ src/test/java/com/borges/api_complete/ApiCompleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borges.api_complete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCompleteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
